89ebaa673cdc9dd64ad6d8ba5bcab17fb63e7ebe
tanne555 <tanne555@googlemail.com>
2017-02-07 22:57:33 +0100

    Peer-graded assignment for R Programming Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# This function serves as a container for a matrix object that can
+# cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) inverse <<- inv
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# This function computes the inverse of a matrix container object
+# created by makeCacheMatrix. If the inverse has already been
+# computed, it retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
